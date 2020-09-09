cd8f01bde72a9eaacbaeecbc7fcdd6cec2176225
AJReading <andy@castlegateit.co.uk>
2020-09-09 11:41:08 +0100

    Added GitHub plugin updater header to plugin file comment.

--- cgit-wp-admin-notifications.php
+++ cgit-wp-admin-notifications.php
@@ -3,6 +3,7 @@
 /*
  * Plugin Name: Castlegate IT WP Admin Notifications
  * Plugin URI: https://github.com/castlegateit/cgit-wp-admin-notifications
+ * GitHub Plugin URI: https://github.com/castlegateit/cgit-wp-admin-notifications
  * Description: Prevents admin notifications from being displayed to non-admin users.
  * Author: Castlegate
  * Author URI: https://www.castlegateit.co.uk
